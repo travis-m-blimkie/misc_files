fef92c6e86012aa6829c4059557003a9f928a85e
Travis <travis.m.blimkie@gmail.com>
2019-01-25 11:06:38 -0800

    Working on script.

--- Scripts/map_tutorial.R
+++ Scripts/map_tutorial.R
@@ -1,3 +1,38 @@
 
 # Loops, apply(), and purrr::map()
 
+practice_list <- list(a = 9,
+                      b = 16,
+                      c = 25)
+
+names(practice_list)
+
+
+# for loop method
+for (i in practice_list) {
+  print(i * 2)
+}
+
+
+# for loop, saving the result
+output_list <- list()
+for (i in practice_list) {
+  output_list[i] = sqrt(i)
+}
+
+# Names are not carried over from the original list, but can be added
+names(output_list)
+
+
+# apply() method
+lapply(X = practice_list, sqrt(x))
+
+
+# map method
+map(.x = practice_list, function(x) x * 2)
+
+# More concise map method
+map(practice_list, ~sqrt(.))
+
+# Include use of pipes
+new_list <- practice_list %>% map(~sqrt(.))
